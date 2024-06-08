9b961eec6a44ec060b174d28bbe0020e189a8fd5
João Tiago <tig.j2025@gmail.com>
2024-06-08 16:36:18 -0300

    change images png to webp

--- script/main.js
+++ script/main.js
@@ -113,12 +113,12 @@ window.onscroll = function() {
 function AlteraVersao(){
     var projeto = document.querySelector(".projeto-img");
     var spantexto = document.querySelector(".spantexto")
-    if (projeto.src.match('01.png')){
-        projeto.src = '02.png';
+    if (projeto.src.match('01.webp')){
+        projeto.src = '02.webp';
         projeto.classList.add("small")
         spantexto.innerHTML = "Versão Mobile"
     } else {
-        projeto.src = '01.png';
+        projeto.src = '01.webp';
         projeto.classList.remove("small")
         spantexto.innerHTML = "Versão Desktop"
     }
